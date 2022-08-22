606cfa02a5989fe67a824434129b5f88bd99405a
6359-abdul <111215439+6359-abdul@users.noreply.github.com>
2022-08-22 20:22:58 +0530

    Add files via upload

---
+++ colunm sum of matrix.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10];
+	int i,j,r,c,sum;
+	scanf("%d%d",&r,&c);
+	for(i=0;i<r;i++)
+   {
+   	for(j=0;j<c;j++)
+   	{
+   		scanf("%d",&a[i][j]);
+	   }
+   }
+   for(i=0;i<r;i++)
+   {
+   	sum=0;
+   	for(j=0;j<c;j++)
+   	{
+   		sum=sum+a[j][i];
+	   }
+   	printf("%d column sum=%d\n",1+i,sum);
+   }
+   return 0;
+}
